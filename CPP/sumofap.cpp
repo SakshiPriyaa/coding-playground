e03a62f34a65df4e59909b2b0425aed2889800e9
SakshiPriyaa <spkpriya0503@gmail.com>
2025-07-23 20:22:27 +0530

    update

--- CPP/sumofap.cpp
+++ CPP/sumofap.cpp
@@ -1,7 +1,6 @@
 // Write a code to find sum of  n terms AP in wich first term and last term is given
 #include <iostream>
 using namespace std; 
-
 int main() {
     int a, l, n;
     cout << "Enter the first term (a), last term (l), and number of terms (n): ";
