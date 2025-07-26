dc4851a112cba6466ee6722c90c3d867f07104ad
SakshiPriyaa <spkpriya0503@gmail.com>
2025-07-27 02:13:16 +0530

    update
    pattern

--- CPP/CPP/CPP.cpp
+++ CPP/CPP/CPP.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 void print(int n) {
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j <= i; j++) {
-			cout << i << " ";
+	for (int i = 1; i <=n; i++) {
+		for (int j = 1; j <=n - i + 1; j++) {
+			cout <<"*";
 		}
 		cout << endl;
 	}
